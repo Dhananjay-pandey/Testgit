537dbd1361030315442d16a6cc2989ee3860de15
Dhananjay <dkiescs998@gmail.com>
2021-03-05 18:16:21 +0530

    JDBC java file commited

---
+++ Jdbc.java
@@ -0,0 +1,25 @@
+import java.sql.*; 
+
+class Jdbc{
+public static void main(String args[]){
+try{
+Class.forName("com.mysql.jdbc.Driver");  
+
+Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/student","root","root");
+
+Statement sm = con.createStatement();
+
+ResultSet rs = sm.executeQuery("select * from s1");
+
+while(rs.next())
+
+System.out.println(rs.getInt(1)+" "+rs.getString(2));
+con.close();
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+
+}
+}
